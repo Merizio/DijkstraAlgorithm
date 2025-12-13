775b3b094d1845d3f55c08a36452f1c90e3f24ab
Merizio <meriziofrancisco@gmail.com>
2025-12-13 12:44:01 -0300

    versao 1.0 do dijkstra

--- code/vertice.h
+++ code/vertice.h
@@ -1,5 +1,6 @@
 #ifndef VERTICE_H
 #define VERTICE_H
+#include "warden.h"
 
 typedef struct no No;
 
@@ -22,6 +23,10 @@ int retornaIdNo(No* no);
 void atualizaId(No* no, int id);
 
 No* retornaPaiNo(No* no); //NO* ...
+
+Warden* retornaWarden(No* no);
+
+float relaxeNo(No* n1, No* n2, float d_1to2);
 
 
 #endif
